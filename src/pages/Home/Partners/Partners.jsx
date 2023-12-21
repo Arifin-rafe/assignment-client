8c85a1c65c72fed075a3f34d1b18a24821899225
Arifin-rafe <arafinrafe90@gmail.com>
2023-12-21 09:58:01 +0600

    partner section added

--- src/pages/Home/Partners/Partners.jsx
+++ src/pages/Home/Partners/Partners.jsx
@@ -4,21 +4,20 @@ import Marquee from "react-fast-marquee";
 
 const Partners = () => {
     return (
-        <div className="mt-10 text-center bg-orange-100">
+        <div className="mt-10 text-center bg-orange-200">
             <h1 className="text-4xl font-serif">Students Universities that collaborate
             <br /> with us before</h1>
 
             <div className="">
                 <Marquee>
+                    
                     <img className="h-56 w-56" src="../../../../public/images/sunway.jpg" alt="" />
                     <img className="h-56 w-56" src="../../../../public/images/city.jpg" alt="" />
-                    <img className="h-56 w-56" src="../../../../public/images/first.jpg" alt="" />
-                    
-                    <img className="h-36 w-56" src="../../../../public/images/ucsi.jpg" alt="" />
-                    
-                    <img className="h-56 w-56" src="../../../../public/images/taylor.jpg" alt="" />
-                    
+                    <img className="h-56 w-56" src="../../../../public/images/first.jpg" alt="" />          
+                    <img className="h-36 w-56" src="../../../../public/images/ucsi.jpg" alt="" />           
+                    <img className="h-56 w-56" src="../../../../public/images/taylor.jpg" alt="" />    
                     <img className="h-56 w-56" src="../../../../public/images/wou.jpg" alt="" />
+                    
                     
                 </Marquee>
             </div>
